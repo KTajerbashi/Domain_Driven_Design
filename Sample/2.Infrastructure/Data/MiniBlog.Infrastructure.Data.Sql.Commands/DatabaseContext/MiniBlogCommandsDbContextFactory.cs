e766c258ccb694eddd861a3dc25c8d6c6223b8ac
KTajerbsahi <kamrantajerbashi@gmail.com>
2024-05-23 00:56:09 +0330

    Feature  : Create Database By Sample Project

---
+++ Sample/2.Infrastructure/Data/MiniBlog.Infrastructure.Data.Sql.Commands/DatabaseContext/MiniBlogCommandsDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace MiniBlog.Infrastructure.Data.Sql.Commands.DatabaseContext
+{
+    public class MiniBlogCommandsDbContextFactory : IDesignTimeDbContextFactory<MiniBlogCommandsDbContext>
+    {
+        public MiniBlogCommandsDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<MiniBlogCommandsDbContext>();
+            optionsBuilder.UseSqlServer("Server =TAJERBASHI; Database=MiniBlogDb;User Id = sa;Password=123123; MultipleActiveResultSets=true; Encrypt = false");
+
+            return new MiniBlogCommandsDbContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ Sample/2.Infrastructure/Data/MiniBlog.Infrastructure.Data.Sql.Commands/Migrations/20240522195935_Initial_Command_Database.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MiniBlog.Infrastructure.Data.Sql.Commands.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial_Command_Database : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Test");
+
+            migrationBuilder.CreateTable(
+                name: "People",
+                schema: "Test",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedByUserId = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    CreatedDateTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifiedByUserId = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    ModifiedDateTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    BusinessId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_People", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "People",
+                schema: "Test");
+        }
+    }
+}
