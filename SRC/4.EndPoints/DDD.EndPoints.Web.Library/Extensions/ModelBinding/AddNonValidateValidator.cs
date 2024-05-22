99a486d7d25f91d685d2b36f4cde199fc532296c
KTajerbsahi <kamrantajerbashi@gmail.com>
2024-05-22 23:53:47 +0330

    Feature : Add Extensions For EndPoint and Application

---
+++ SRC/4.EndPoints/DDD.EndPoints.Web.Library/Extensions/ModelBinding/AddNonValidateValidator.cs
@@ -0,0 +1,17 @@
+﻿using DDD.EndPoints.Web.Library.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+namespace DDD.EndPoints.Web.Library.Extensions.ModelBinding;
+
+public static class NonValidatingValidatorExtensions
+{
+    public static IServiceCollection AddNonValidatingValidator(this IServiceCollection services)
+    {
+        var validator = services.FirstOrDefault(s => s.ServiceType == typeof(IObjectModelValidator));
+        if (validator != null)
+        {
+            services.Remove(validator);
+            services.Add(new ServiceDescriptor(typeof(IObjectModelValidator), _ => new NonValidatingValidator(), ServiceLifetime.Singleton));
+        }
+        return services;
+    }
+}
