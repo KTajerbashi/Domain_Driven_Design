9aaa0e92d9a1e3a65aaec59065db55efe158183c
Tajerbashi <kamrantajerbashi@gmail.com>
2024-06-09 22:00:32 +0330

    Add Shared Kernel

---
+++ SharedKernel/Extensions.Abstractions/Extensions.Chaching.Abstractions/FakeCacheAdapter.cs
@@ -0,0 +1,10 @@
+﻿namespace Extensions.Chaching.Abstractions;
+
+public class FakeCacheAdapter : ICacheAdapter
+{
+    public void Add<TInput>(string key, TInput obj, DateTime? AbsoluteExpiration, TimeSpan? SlidingExpiration) { }
+
+    public TOutput Get<TOutput>(string key) => default;
+
+    public void RemoveCache(string key) { }
+}
---
+++ SharedKernel/Extensions.Abstractions/Extensions.ChangeDataLog.Abstractions/DatabaseChangeType.cs
@@ -0,0 +1,7 @@
+﻿namespace Extensions.ChangeDataLog.Abstractions;
+public enum DatabaseChangeType
+{
+    Insert = 1,
+    Update = 2,
+    Delete = 3
+}
