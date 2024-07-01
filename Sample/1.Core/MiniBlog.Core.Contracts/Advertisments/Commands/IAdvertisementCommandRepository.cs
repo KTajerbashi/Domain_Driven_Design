9b2412b5fd678c30dadb14e51fab3b36cc426536
Tajerbashi <kamrantajerbashi@gmail.com>
2024-07-01 17:54:28 +0330

    Feature : Add Reposiories

--- Sample/1.Core/MiniBlog.Core.Contracts/Advertisments/Commands/IAdvertisementCommandRepository.cs
+++ Sample/1.Core/MiniBlog.Core.Contracts/Advertisments/Commands/IAdvertisementCommandRepository.cs
@@ -1,13 +1,12 @@
 ﻿using DDD.Core.Contracts.Library.Data.Commands;
 using MiniBlog.Core.Domain.Advertisements.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MiniBlog.Core.Contracts.Advertisments.Commands;
 
-public interface IAdvertisementCommandRepository:ICommandRepository<Advertisement,long>
+public interface IAdvertisementCommandRepository : ICommandRepository<Advertisement, long>
 {
+    long CreateAdmin(Admin entity);
+    long CreateCourse(Course entity);
+    Course UpdateCourse(Course entity);
+    bool DeleteCourse(Course entity);
 }
