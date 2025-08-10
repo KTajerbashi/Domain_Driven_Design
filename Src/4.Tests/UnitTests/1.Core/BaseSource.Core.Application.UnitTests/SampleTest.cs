d5bb8db47127b6fb031ab22b0211e6affbfe3705
KTajerbashi <kamrantajerbashi@gmail.com>
2025-08-10 13:03:17 +0330

    Feature Add Integration and Unit Tests Solutions

---
+++ Src/4.Tests/UnitTests/1.Core/BaseSource.Core.Application.UnitTests/SampleTest.cs
@@ -0,0 +1,15 @@
+﻿namespace BaseSource.Core.Application.UnitTests;
+
+public class SampleTest
+{
+    [Fact]
+    public void Test1()
+    {
+        //  Arrange
+        int a = 10, b = 20, c = 0;
+        //  Act
+        c = a + b;
+        //  Assert
+        Assert.Equal(30, c);
+    }
+}
