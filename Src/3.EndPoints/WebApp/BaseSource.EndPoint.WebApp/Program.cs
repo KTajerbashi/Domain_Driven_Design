49180bf6ca6a9cd733683cd6cdbf1c0614e465a5
KTajerbashi <kamrantajerbashi@gmail.com>
2025-08-10 12:06:48 +0330

    Feature : Add Css Styles

--- Src/3.EndPoints/WebApp/BaseSource.EndPoint.WebApp/Program.cs
+++ Src/3.EndPoints/WebApp/BaseSource.EndPoint.WebApp/Program.cs
@@ -1,15 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+
+builder.Services.AddControllers();
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -20,7 +20,13 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapStaticAssets();
+
+app.MapControllers();
+
+// Map Razor Pages (with /App prefix thanks to convention)
 app.MapRazorPages()
    .WithStaticAssets();
+
+app.MapGet("/", () => Results.Redirect("/App"));
 
 app.Run();
