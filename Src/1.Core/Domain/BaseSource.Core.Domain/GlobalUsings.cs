1b14c3d08888ec11b7d9400e5577e46f16c3b4ad
KTajerbashi <kamrantajerbashi@gmail.com>
2025-08-11 13:55:55 +0330

    Fix .

--- Src/1.Core/Domain/BaseSource.Core.Domain/GlobalUsings.cs
+++ Src/1.Core/Domain/BaseSource.Core.Domain/GlobalUsings.cs
@@ -1,4 +1,2 @@
-﻿global using BaseSource.Core.Domain.Common.Aggregate.Implementions;
-global using BaseSource.Core.Domain.Common.Aggregate.Interfaces;
-global using BaseSource.Core.Domain.Common.Event;
+﻿global using BaseSource.Core.Domain.Common.Event;
 global using BaseSource.Core.Domain.Common;
