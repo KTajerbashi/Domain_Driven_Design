a74a8d27e07a179c7bd8497c1628e664dec4124c
KTajerbashi <kamrantajerbashi@gmail.com>
2025-08-14 23:10:52 +0330

    #Fix UnitOfWork Pattern

--- Src/1.Core/Application/BaseSource.Core.Application/Common/Repositories/ICommandRepository.cs
+++ Src/1.Core/Application/BaseSource.Core.Application/Common/Repositories/ICommandRepository.cs
@@ -1,9 +1,10 @@
-﻿using BaseSource.Core.Domain.Common.Aggregate;
+﻿using BaseSource.Core.Application.Common.Patterns;
+using BaseSource.Core.Domain.Common.Aggregate;
 using BaseSource.Core.Domain.Common.ValueObjects;
 using System.Linq.Expressions;
 
 namespace BaseSource.Core.Application.Common.Repositories;
-public interface ICommandRepository<TEntity, TId>
+public interface ICommandRepository<TEntity, TId> : IUnitOfWork
     where TEntity : AggregateRoot<TId>
     where TId : struct,
           IComparable,
@@ -13,12 +14,12 @@ public interface ICommandRepository<TEntity, TId>
           IFormattable
 {
     Task AddAsync(TEntity entity, CancellationToken cancellation = default);
-    
+
     Task<TEntity> GetAsync(TId id, CancellationToken cancellation = default);
     Task<TEntity> GetGraphAsync(TId id, CancellationToken cancellation = default);
     Task<TEntity> GetAsync(EntityId entityId, CancellationToken cancellation = default);
     Task<IEnumerable<TEntity>> GetAsync(CancellationToken cancellation = default);
-    
+
     Task DeleteAsync(TId id, CancellationToken cancellation = default);
     Task DeleteAsync(EntityId entityId, CancellationToken cancellation = default);
     Task DeleteGraphAsync(EntityId entityId, CancellationToken cancellation = default);
