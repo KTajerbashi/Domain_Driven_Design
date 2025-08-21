955a00b4b3b9ad1fcee5df7de353de66e139bc4d
KTajerbashi <kamrantajerbashi@gmail.com>
2025-08-21 23:55:22 +0330

    #Feature Add Application Product and Order Repositories to Test flow of software architecture with patterns

---
+++ Src/1.Core/Application/BaseSource.Core.Application/UseCases/Store/Products/Repositories/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using BaseSource.Core.Application.Common.Repositories;
+using BaseSource.Core.Domain.Aggregates.Store.Products.Entities;
+
+namespace BaseSource.Core.Application.UseCases.Store.Products.Repositories;
+
+public interface IProductCRepository : ICommandRepository<Product, long>
+{
+}
+public interface IProductQRepository : IQueryRepository<Product, long>
+{
+}
